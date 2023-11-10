cb5ba38f16f9bac0a8b182ef8e1bbc24c391a538
Kirill Muzychenko <kirillmuz@yandex.ru>
2023-11-11 01:34:48 +0300

    #COD-40 Правка ошибок тестов

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,8 +1,9 @@
-import App from './App'
+import { App } from './App'
 import { render, screen } from '@testing-library/react'
 
-const appContent = 'Вот тут будет жить ваше приложение :)'
+const appContent = 'The coder game!'
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
