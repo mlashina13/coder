c731fbdb4d32302863103390dcc0d604b574f138
Mariia <ams_l@mail.ru>
2023-12-04 14:08:38 +0300

    fix bug

--- packages/client/src/components/LeaderBordList/LeaderBordItem/LeaderBordItem.tsx
+++ packages/client/src/components/LeaderBordList/LeaderBordItem/LeaderBordItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { ListItem, ListItemText } from '@mui/material';
 import './LeaderBordItemStyles.scss';
 import { LeaderBordItemProps } from './LeaderBordItemProps';
