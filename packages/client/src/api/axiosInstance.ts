6af4a275299a170684b3dc7f6f03375686102424
Kirill Muzychenko <kirillmuz@yandex.ru>
2024-02-06 14:19:11 +0300

    Проверка фикса хоста

--- packages/client/src/api/axiosInstance.ts
+++ packages/client/src/api/axiosInstance.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
-const { SERVER_HOST } = process.env;
+// const { SERVER_HOST } = process.env;
 
-const serverUrl = SERVER_HOST ?? 'localhost';
+// const serverUrl = SERVER_HOST ?? 'localhost';
+
+const baseURL = 'https://coder-32.ya-praktikum.tech/api';
 
 export const axiosInstance = axios.create({
-  baseURL: `http://${serverUrl}:3001/api`,
+  // baseURL: `http://${serverUrl}:3001/api`,
+  baseURL,
   withCredentials: true,
 });
